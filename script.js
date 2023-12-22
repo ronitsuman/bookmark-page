6ff264e06d5b844bf9ea4d90a91640275eabc5bf
Ronit Suman <ronitsuman59@gmail.com>
2023-12-22 21:42:37 +0530

    hi

---
+++ script.js
@@ -0,0 +1,17 @@
+// function sendMessage(user) {
+//     var messageInput = document.getElementById(user + "-message-input");
+//     var chatBox = document.getElementById(user + "-chat");
+
+//     var messageText = messageInput.value;
+//     if (messageText.trim() !== "") {
+//         var newMessage = document.createElement("div");
+//         newMessage.className = "message";
+//         newMessage.innerHTML = "<strong>" + user + ":</strong> " + messageText;
+//         chatBox.appendChild(newMessage);
+
+//         // Clear the input field after sending the message
+//         messageInput.value = "";
+//     }
+// }
+
+
